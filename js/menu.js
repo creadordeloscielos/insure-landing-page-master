5ccae2833ba944c47dc034b3b30a388d9d8288e3
salvador galvan hernadez <vida.suero.oral.18@gmail.com>
2021-02-14 22:13:47 -0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById ("btn");
+let menu = document.getElementById("menu");
+contador = 0;
+
+
+
+btn.addEventListener("click", function(){ 
+
+    if (contador == 0){
+        
+    menu.className = ("mostrar");   
+    contador = 1;
+
+    } else {
+        menu.classList.remove("mostrar");
+        contador = 0;
+    }
+
+
+
+})
+
+window.addEventListener('resize', function (){
+  
+    if(screen.width > 870){
+        
+        menu.classList.remove("mostrar");
+        contador = 0;
+
+    }
+
+
+})
